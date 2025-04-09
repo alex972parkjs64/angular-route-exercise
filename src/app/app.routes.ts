8b127de238fe1be105eb40e5fe05021822f8e0f5
Alex <59156225+alex972parkjs64@users.noreply.github.com>
2025-04-09 12:58:39 -0500

    adding base routing code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const ROUTES: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
